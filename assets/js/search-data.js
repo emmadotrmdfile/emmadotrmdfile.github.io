3a17e3cba4960ea8029f869bfda422ec94e353fd
emmadotrmdfile <huizen32@msu.edu>
2026-03-27 01:19:47 +0000

    Deploying to gh-pages from @ emmadotrmdfile/emmadotrmdfile.github.io@c9b5e9f758f2e08beee3ca92ee288c55db0f1d3a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,12 +445,12 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
-          description: "a project with a background image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
-          description: "a project with no image",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
