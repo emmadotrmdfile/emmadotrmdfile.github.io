8a561ad6fa321ea4eed6d27a59c29210ce208f0f
emmadotrmdfile <huizen32@msu.edu>
2026-03-29 14:57:54 +0000

    Deploying to gh-pages from @ emmadotrmdfile/emmadotrmdfile.github.io@3355b23bb310faf9de7206d0c108080cc2488db7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "",
@@ -39,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
