78aaf228805afe8d7f662146be1b3e0cec32500e
emmadotrmdfile <huizen32@msu.edu>
2026-06-23 01:53:33 +0000

    Deploying to gh-pages from @ emmadotrmdfile/emmadotrmdfile.github.io@e259788419d2fdf2ae253caabc533c07c8decbca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-vitae",
-          title: "vitae",
+        },{id: "nav-cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
